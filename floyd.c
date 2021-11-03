41c830b054375f16b7985c135749721e1ae718ac
Gabriele Tramonte <gabry.t92@gmail.com>
2021-11-03 19:37:53 +0100

    define macros

--- floyd.c
+++ floyd.c
@@ -5,6 +5,17 @@
 
 #include <mpi.h>
 #include <stdio.h>
+#include <malloc.h>
+
+
+#define MIN(a,b)           ((a)<(b)?(a):(b))
+#define BLOCK_LOW(id,p,n)  ((id)*(n)/(p))
+#define BLOCK_HIGH(id,p,n) (BLOCK_LOW((id)+1,p,n)-1)
+#define BLOCK_SIZE(id,p,n) \
+                     (BLOCK_HIGH(id,p,n)-BLOCK_LOW(id,p,n)+1)
+#define BLOCK_OWNER(j,p,n) (((p)*((j)+1)-1)/(n))
+#define PTR_SIZE           (sizeof(void*))
+#define CEILING(i,j)       (((i)+(j)-1)/(j))
 
 int main(int argc, char **argv){
 
@@ -12,12 +23,26 @@ int main(int argc, char **argv){
     int processes;
 
     int **matrix, *matrix_storage, i;
-    int m,n;
+    int n;
+
+    FILE* matrix_file;
 
     MPI_Init(&argc,&argv);
     MPI_Comm_rank(MPI_COMM_WORLD,&rank);
     MPI_Comm_size(MPI_COMM_WORLD,&processes);
 
+
+    if(!rank) {
+        matrix_file = fopen("../matrix","r");
+        fscanf(matrix_file,"%d",n);
+
+        matrix_storage = (int *) malloc(n * sizeof(int));
+        matrix = (int **) malloc(n * n * sizeof(int));
+        for (i = 0; i < n; i++) matrix[i] = &matrix_storage[i * n];
+
+
+
+    }
 
 
     MPI_Finalize();
