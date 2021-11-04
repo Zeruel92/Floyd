92cec5142d319952606a51bb27c452d31c3233d1
Gabriele Tramonte <gabry.t92@gmail.com>
2021-11-04 20:06:01 +0100

    floyd parallel implementation with debug prints

--- floyd.c
+++ floyd.c
@@ -6,38 +6,45 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 
-#define MIN(a,b)           ((a)<(b)?(a):(b))
-#define BLOCK_LOW(id,p,n)  ((id)*(n)/(p))
-#define BLOCK_HIGH(id,p,n) (BLOCK_LOW((id)+1,p,n)-1)
-#define BLOCK_SIZE(id,p,n) (BLOCK_HIGH(id,p,n)-BLOCK_LOW(id,p,n)+1)
-#define BLOCK_OWNER(j,p,n) (((p)*((j)+1)-1)/(n))
-#define PTR_SIZE           (sizeof(void*))
-#define CEILING(i,j)       (((i)+(j)-1)/(j))
+#define MIN(a, b)           ((a)<(b)?(a):(b))
+#define BLOCK_LOW(id, p, n)  ((id)*(n)/(p))
+#define BLOCK_HIGH(id, p, n) (BLOCK_LOW((id)+1,p,n)-1)
+#define BLOCK_SIZE(id, p, n) (BLOCK_HIGH(id,p,n)-BLOCK_LOW(id,p,n)+1)
+#define BLOCK_OWNER(j, p, n) (((p)*((j)+1)-1)/(n))
 
-int main(int argc, char **argv){
+#define MAX_ITERATIONS 1
+
+int main(int argc, char **argv) {
 
     int rank;
     int processes;
 
-    int **matrix, *matrix_storage;
+    int **matrix, *matrix_storage, *k_row;
     int n;
     int size;
-    char buffer[255];
 
-    MPI_Status  status;
+    double elapsed_time;
 
-    FILE* matrix_file;
+    MPI_Status status;
+    MPI_Request request;
 
-    MPI_Init(&argc,&argv);
-    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-    MPI_Comm_size(MPI_COMM_WORLD,&processes);
+    FILE *matrix_file;
+
+#ifdef _DEBUG
+    int debug = 1;
+#endif
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &processes);
 
     // Reading matrix size
-    if(rank==(processes-1)) {
-        matrix_file = fopen("./matrix","r");
-        fscanf(matrix_file,"%d",&n);
+    if (rank == (processes - 1)) {
+        matrix_file = fopen("./matrix", "r");
+        fscanf(matrix_file, "%d", &n);
         //n = strtol(buffer,NULL,10);
     }
     MPI_Bcast(&n,1,MPI_INT,processes-1,MPI_COMM_WORLD);
@@ -60,32 +67,114 @@ int main(int argc, char **argv){
 
     //Loading array
     if(rank == processes-1){
-        for(int i = 0; i < processes-1; i++){
-            int local_size = BLOCK_SIZE(i,processes,n);
-          //  fread(matrix_storage,sizeof(int),local_size*n,matrix_file);
-            for(int i = 0; i < local_size*n; i++)
-                fscanf(matrix_file,"%d",&matrix_storage[i]);
-            MPI_Send(matrix_storage,local_size*n,MPI_INT,i,0,MPI_COMM_WORLD);
+        for(int i = 0; i < processes-1; i++) {
+            int local_size = BLOCK_SIZE(i, processes, n);
+            for (int j = 0; j < local_size * n; j++)
+                fscanf(matrix_file, "%d", &matrix_storage[j]);
+            MPI_Irsend(matrix_storage, local_size * n, MPI_INT, i, 0, MPI_COMM_WORLD, &request);
         }
-        for(int i = 0; i < size*n; i++)
-            fscanf(matrix_file,"%d",&matrix_storage[i]);
-    } else{
-        MPI_Recv(matrix_storage,size*n,MPI_INT,processes-1, 0,MPI_COMM_WORLD, &status);
+        for (int i = 0; i < size * n; i++)
+            fscanf(matrix_file, "%d", &matrix_storage[i]);
+    } else {
+        MPI_Recv(matrix_storage, size * n, MPI_INT, processes - 1, 0, MPI_COMM_WORLD, &status);
     }
 
 #ifdef _DEBUG
-    fprintf(stdout,"Process: %d\n",rank);
-    for(int i =0; i <size; i ++){
-        for(int k =0; k<n; k++){
-            fprintf(stdout,"%d ",matrix[i][k]);
-        }
-        fprintf(stdout,"\n");
+
+    if (!rank) {
+        fprintf(stdout, "INPUT MATRIX:\n");
+        fflush(stdout);
+    } else {
+        MPI_Recv(&debug, 1, MPI_INT, (rank - 1) % processes, 1, MPI_COMM_WORLD, &status);
     }
+    for (int i = 0; i < size; i++) {
+        for (int k = 0; k < n; k++) {
+            fprintf(stdout, "%d ", matrix[i][k]);
+            fflush(stdout);
+        }
+        fprintf(stdout, "\n");
+        fflush(stdout);
+    }
+    MPI_Send(&debug, 1, MPI_INT, (rank + 1) % processes, 1, MPI_COMM_WORLD);
 #endif
 
+    k_row = (int *) malloc(sizeof(int) * n);
+
+    //Starting computation
+    MPI_Barrier(MPI_COMM_WORLD);
+    elapsed_time = -MPI_Wtime();
+
+    for (int iterations = 0; iterations < MAX_ITERATIONS; iterations++) {
+
+        for (int k = 0; k < n; k++) {
+            int owner = BLOCK_OWNER(k, processes, n);
+            if (rank == owner) {
+                for (int j = 0; j < n; j++)
+                    k_row[j] = matrix_storage[(k % size) * n + j];
+            }
+            MPI_Bcast(k_row, n, MPI_INT, owner, MPI_COMM_WORLD);
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < n; j++) {
+                    matrix[i][j] = MIN(matrix[i][j], matrix[i][k] + k_row[j]);
+                }
+            }
+#ifdef _DEBUG
+            sleep(1);
+            if (!rank) {
+                fprintf(stdout, "K ROW\n");
+                fflush(stdout);
+                for (int j = 0; j < n; j++) {
+                    fprintf(stdout, "%d ", k_row[j]);
+                    fflush(stdout);
+                }
+                fprintf(stdout, "\nIntermediate MATRIX:\n");
+                fflush(stdout);
+            } else {
+                MPI_Recv(&debug, 1, MPI_INT, (rank - 1) % processes, 1, MPI_COMM_WORLD, &status);
+            }
+            for (int i = 0; i < size; i++) {
+                for (int k = 0; k < n; k++) {
+                    fprintf(stdout, "%d ", matrix[i][k]);
+                    fflush(stdout);
+                }
+                fprintf(stdout, "\n");
+                fflush(stdout);
+            }
+            MPI_Send(&debug, 1, MPI_INT, (rank + 1) % processes, 1, MPI_COMM_WORLD);
+#endif
+        }
+    }
+
+#ifdef _DEBUG
+    sleep(1);
+    if (!rank) {
+        fprintf(stdout, "OUTPUT MATRIX:\n");
+        fflush(stdout);
+    } else {
+        MPI_Recv(&debug, 1, MPI_INT, (rank - 1) % processes, 1, MPI_COMM_WORLD, &status);
+    }
+    for (int i = 0; i < size; i++) {
+        for (int k = 0; k < n; k++) {
+            fprintf(stdout, "%d ", matrix[i][k]);
+            fflush(stdout);
+        }
+        fprintf(stdout, "\n");
+        fflush(stdout);
+    }
+    MPI_Send(&debug, 1, MPI_INT, (rank + 1) % processes, 1, MPI_COMM_WORLD);
+#endif
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    elapsed_time += MPI_Wtime();
+
+    elapsed_time /= MAX_ITERATIONS;
+
+    if (!rank) fprintf(stdout, "Processors: %d Iterations: %d, Time: %f", processes, MAX_ITERATIONS, elapsed_time);
+
     // Free matrix and exit
-    free(matrix),matrix=NULL;
-    free(matrix_storage),matrix_storage=NULL;
+    free(matrix), matrix = NULL;
+    free(matrix_storage), matrix_storage = NULL;
+    free(k_row), k_row = NULL;
 
     MPI_Finalize();
     return 0;
